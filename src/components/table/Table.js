4b7790c32fc8484cbb59bb767057aa56b6b83180
OlaIstra <elhazne@gmail.com>
2020-05-12 11:20:11 +0300

    added resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,19 +1,35 @@
 import { AppComponent } from "@core/AppComponent";
 import { createTable } from "./table.template";
+import { handlerResize } from "./resize";
+import { shouldResize } from "./helpers";
 
 export class Table extends AppComponent {
-  //static className = "app__table";
+	//static className = "app__table";
 
-  constructor($root) {
-    super($root);
-    this.className = "app__table";
-  }
+	constructor($root) {
+		super($root, {
+			listeners: ["mousedown"],
+		});
+		this.className = "app__table";
+	}
 
-  getClassName() {
-    return this.className;
-  }
+	getClassName() {
+		return this.className;
+	}
 
-  toHTML() {
-    return createTable();
-  }
+	toHTML() {
+		return createTable(20);
+	}
+
+	onClick() {}
+
+	onMousedown(event) {
+		if (shouldResize(event)) {
+			handlerResize(event, this.$root);
+		}
+	}
+
+	onMousemove() {}
+
+	onMouseup() {}
 }
